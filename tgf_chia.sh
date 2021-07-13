707b742aa3dfd9d51a1afa82a5072dadb5a229cb
rom <you@example.com>
2021-07-14 00:51:25 +0200

    update script to multiple harveters

--- tgf_chia.sh
+++ tgf_chia.sh
@@ -9,9 +9,11 @@ export PATH=$PATH':/home'
 readarray -t arr <<<$(/home/chia-blockchain/venv/bin/chia farm summary)
 unset -v 'arr[5]'
 unset -v 'arr[6]'
-Expectedtimetowin=$(echo ${arr[10]} | cut -d' ' -f 5)
-arr[10]=$(echo 'Expectedtimetowin='$(($Expectedtimetowin+1)))
-unset -v 'arr[11]'
+unset -v 'arr[7]'
+unset -v 'arr[8]'
+Expectedtimetowin=$(echo ${arr[12]} | cut -d' ' -f 5)
+arr[12]=$(echo 'Expectedtimetowin='$(($Expectedtimetowin+1)))
+unset -v 'arr[13]'
 
 for i in ${!arr[@]}; do
 	data=$(echo $data','${arr[$i]} | sed 's/:/=/;s/ C//;s/[[:space:]]//g')
@@ -24,4 +26,3 @@ chiadata=$(echo $chiadata | sed 's/,Farmingstatus/Farmingstatus/;s/Farming,/1,/;
 deactivate
 
 echo $chiadata
-
