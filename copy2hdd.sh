a8a7f627ba651d1af6ece864d959925d4db78493
rom <you@example.com>
2021-07-14 00:55:12 +0200

    copy script

--- copy2hdd.sh
+++ copy2hdd.sh
@@ -4,12 +4,12 @@
 if ps -ef | grep -v grep | grep "mv /mnt/" ; then
     echo "already running" && exit 0
 fi
-##check which disk is free
-##target=$(df /mnt/hdd? | tail -n +2 | sort -nk 4 | tail -1 | head -n1 | awk '{print $6;}')
-#df=$(df -h /mnt/hdd? | tail -n +2 | sort -hk 4 | tail -1)
-#target=$(echo $df | awk '{print($6)}')
-#avl=$(echo $df | awk '{print($4)}')
-target=$(echo "/mnt/hdd"$(($RANDOM % 10))"/")
+#check which disk is free
+df=$(df -h /mnt/hdd? | tail -n +2 | sort -hk 4 | tail -1)
+target=$(echo $df | awk '{print($6)}')
+avl=$(echo $df | awk '{print($4)}')
+##or if you want just a random drive
+#target=$(echo "/mnt/hdd"$(($RANDOM % 10))"/")
 echo "target disk: ${target}, available: ${avl}"
 
 if [ `ls -A /mnt/md3/plots/ | wc -l` == "0" ]; then
@@ -32,17 +32,6 @@ if [ `ls -A /mnt/md4/plots/ | wc -l` == "0" ]; then
       copy=$(ls /mnt/md4/plots/ | head -1)
       echo "copy file ${copy} to ${target}/pool/"
       mv /mnt/md4/plots/$copy $target/pool/
-fi
-
-if [ `ls -A /mnt/md2/plots/ | wc -l` == "0" ]; then
-      echo "md2/plots is empty"
-  else
-      delete=$(ls $target/plot-k32-* | head -1)
-      echo "deleting file ${delete}"
-      rm $delete
-      copy=$(ls /mnt/md2/plots/ | head -1)
-      echo "copy file ${copy} to ${target}/pool/"
-      mv /mnt/md2/plots/$copy $target/pool/
 fi
 
 echo 'copy completed'
